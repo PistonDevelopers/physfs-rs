8889e435edd1e183dc0f7434f044303f9f4e0036
Steve Klabnik <steve@steveklabnik.com>
2014-10-30 00:24:47 -0400

    fail -> panic

--- tests/directory/mod.rs
+++ tests/directory/mod.rs
@@ -4,27 +4,27 @@ use physfs::file;
 #[test]
 fn read_file_from_directory() {
     let con = match PhysFSContext::new() {
-        Err(msg) => fail!(msg),
+        Err(msg) => panic!(msg),
         Ok(con) => con
     };
 
     assert!(PhysFSContext::is_init());
 
     match con.mount(super::path_to_here.to_string(), "/test/".to_string(), true) {
-        Err(msg) => fail!(msg),
+        Err(msg) => panic!(msg),
         _ => {}
     }
 
     let file = match file::File::open(&con, "/test/directory/read.txt".to_string(), file::Read) {
         Ok(f) => f,
-        Err(msg) => fail!(msg)
+        Err(msg) => panic!(msg)
     };
 
     let mut bytes = [0u8, ..32];
     let buf = bytes.as_mut_slice();
 
     match file.read(buf, 1, 32) {
-        Err(msg) => fail!(msg),
+        Err(msg) => panic!(msg),
         _ => {}
     }
 
