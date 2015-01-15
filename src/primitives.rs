bb088d3fbed4a239464463531b761495893578d4
panicbit <panicbit@users.noreply.github.com>
2015-01-15 02:52:57 +0100

    Update `cfg(target_word_size)`

--- src/primitives.rs
+++ src/primitives.rs
@@ -7,13 +7,13 @@ pub type PHYSFS_sint16 = ::libc::c_short;
 pub type PHYSFS_uint32 = ::libc::c_uint;
 pub type PHYSFS_sint32 = ::libc::c_int;
 
-#[cfg(target_word_size = "64")] 
+#[cfg(target_pointer_width = "64")]
 pub type PHYSFS_uint64 = ::libc::c_ulonglong;
-#[cfg(target_word_size = "64")] 
+#[cfg(target_pointer_width = "64")]
 pub type PHYSFS_sint64 = ::libc::c_longlong;
 
 //PhysFS defines the 64-bit types to 32 bits on 32-bit systems.
-#[cfg(target_word_size = "32")]
+#[cfg(target_pointer_width = "32")]
 pub type PHYSFS_uint64 = ::libc::c_uint;
-#[cfg(target_word_size = "32")]
+#[cfg(target_pointer_width = "32")]
 pub type PHYSFS_sint64 = ::libc::c_int;
